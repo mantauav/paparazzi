b5cd449b3b4b1241a0b9edc1d78801c121420bdd
skate <skate@aurora-CF-19Wings.(none)>
2011-05-03 17:29:12 -0400

    added skatestart.sh script, joystick id setting

---
+++ scripts/skatestart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/skate/skate/paparazzi
+source ../scripts/aliases.txt
+export PAPARAZZI_HOME=/home/skate/skate/paparazzi
+export PAPARAZZI_SRC=/home/skate/skate/paparazzi
+#sudo ifconfig eth1 192.168.0.2/24 up
+#skifup
+#setpap
+./paparazzi -session "Flight - Skate @ 57600 - Joystick"
