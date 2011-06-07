648a54db24e48f8fee4c746b707c02b6a2397221
Andre Devitt <andredevitt@gmail.com>
2011-06-07 14:18:34 -0400

    fix autostart scripts with new dir structure

--- scripts/skatestart.sh
+++ scripts/skatestart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /home/skate/skate/paparazzi
-source ../scripts/aliases.txt
-export PAPARAZZI_HOME=/home/skate/skate/paparazzi
-export PAPARAZZI_SRC=/home/skate/skate/paparazzi
+cd /home/skate/skate/
+source ./scripts/aliases.txt
+export PAPARAZZI_HOME=/home/skate/skate/
+export PAPARAZZI_SRC=/home/skate/skate/
 #sudo ifconfig eth1 192.168.0.2/24 up
 #skifup
 #setpap
